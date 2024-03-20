c15474ddf15fb4255cdf47546a55122c70289557
ngrwisnu <adiprayogongrwisnu@gmail.com>
2024-03-21 02:50:04 +0700

    [update] add more assertions in ThreadDetails test to deep check the output

--- src/Domains/threads/entities/_test/ThreadDetails.test.js
+++ src/Domains/threads/entities/_test/ThreadDetails.test.js
@@ -36,14 +36,41 @@ describe("ThreadDetails", () => {
 
     const result = new ThreadDetails(thread, comments, replies);
 
-    console.log(result);
     expect(result.id).toBe(thread.id);
     expect(result.title).toBe(thread.title);
     expect(result.body).toBe(thread.body);
     expect(result.username).toBe(thread.username);
     expect(result.date).toBe(new Date(+thread.date));
     expect(result.comments.length).toBe(1);
+    expect(result.comments[0]).toHaveProperty("id", comments[0].id);
+    expect(result.comments[0]).toHaveProperty("content", comments[0].content);
+    expect(result.comments[0]).toHaveProperty("username", comments[0].username);
+    expect(result.comments[0]).not.toHaveProperty(
+      "is_deleted",
+      comments[0].is_deleted
+    );
+    expect(result.comments[0]).toHaveProperty(
+      "date",
+      new Date(+comments[0].date)
+    );
     expect(result.comments[0].replies.length).toBe(1);
+    expect(result.comments[0].replies[0]).toHaveProperty("id", replies[0].id);
+    expect(result.comments[0].replies[0]).toHaveProperty(
+      "content",
+      replies[0].content
+    );
+    expect(result.comments[0].replies[0]).toHaveProperty(
+      "username",
+      replies[0].username
+    );
+    expect(result.comments[0].replies[0]).not.toHaveProperty(
+      "is_deleted",
+      replies[0].is_deleted
+    );
+    expect(result.comments[0].replies[0]).toHaveProperty(
+      "date",
+      new Date(+replies[0].date)
+    );
   });
 
   it("should return the correct object with deleted content", async () => {
